5462f31cacb1359d518aace93aa9b9254994017b
Nneji123 <ifeanyinneji777@gmail.com>
2022-08-17 21:44:58 +0100

    final upload

--- setup.py
+++ setup.py
@@ -58,19 +58,19 @@ setup(
     python_requires=">=3.7",
     include_package_data=True,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Natural Language :: English",
         "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Code Generators",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     entry_points="""
     [console_scripts]
